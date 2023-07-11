a482f9af61d20a7958c0966edba681d2368fc630
DESKTOP-T1H0VU0\Nikola <nikolaakv@gmail.com>
2023-07-12 00:08:54 +0200

    Change

--- entity/movie.go
+++ entity/movie.go
@@ -1,11 +1,10 @@
 package entity
 
 type Movie struct {
-	ID            int64   `json:"id"`
-	Title         string  `json:"title"`
-	OriginalTitle string  `json:"originalTitle"`
-	Duration      string  `json:"duration"`
-	ImageUrl      string  `json:"imageUrl"`
-	GenreId       int64   `json:"genreId"`
-	Movies        []Movie `json:"movies"`
+	ID            int64  `json:"id"`
+	Title         string `json:"title"`
+	OriginalTitle string `json:"originalTitle"`
+	Duration      string `json:"duration"`
+	ImageUrl      string `json:"imageUrl"`
+	GenreId       int64  `json:"genreId"`
 }
